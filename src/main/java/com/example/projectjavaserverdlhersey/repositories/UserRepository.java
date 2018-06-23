4817f4156c7c73c1e215e1427dc5a334a8085adf
David Hersey <hersey.da@husky.neu.edu>
2018-06-23 14:52:26 -0700

    fixed CrossOrigin

--- src/main/java/com/example/projectjavaserverdlhersey/repositories/UserRepository.java
+++ src/main/java/com/example/projectjavaserverdlhersey/repositories/UserRepository.java
@@ -1,10 +1,18 @@
 package com.example.projectjavaserverdlhersey.repositories;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.example.projectjavaserverdlhersey.models.User;
 
 public interface UserRepository 
-	extends CrudRepository<User, Integer>{
+	extends CrudRepository<User, Integer> {
+	@Query("SELECT u FROM User u WHERE u.username=:username AND u.password=:password")
+	Optional<User> findUserByCredentials(
+		@Param("username") String username, 
+		@Param("password") String password);
 
 }
--- src/main/java/com/example/projectjavaserverdlhersey/services/UserService.java
+++ src/main/java/com/example/projectjavaserverdlhersey/services/UserService.java
@@ -2,7 +2,11 @@ package com.example.projectjavaserverdlhersey.services;
 
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.projectjavaserverdlhersey.models.User;
 import com.example.projectjavaserverdlhersey.repositories.UserRepository;
 
+
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class UserService {
 	@Autowired
@@ -50,5 +56,23 @@ public class UserService {
 	@PutMapping("/api/user/{uid}")
 	public void deleteUser(@PathVariable("uid") int uid) {
 		repository.deleteById(uid);
+	}
+	
+	//LOGIN
+	@PostMapping("/api/login")
+	public User login(@RequestBody User credentials, HttpSession session) {
+		Optional<User> data = repository.findUserByCredentials(credentials.getUsername(), credentials.getPassword());
+		if(data.isPresent()) {
+			session.setAttribute("currentUser", data.get());
+			return data.get();
+		}
+		return null;
+	}
+	
+
+	//LOGOUT
+	@PostMapping("/api/logout")
+	public void logout(@RequestBody HttpSession session, HttpServletResponse response) {
+			session.invalidate();
 	}
 }
